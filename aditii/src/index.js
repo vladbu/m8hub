2645e0f233e262e5dc279463f5b5c628bb47f7ea
vladbu <vladbuinytski@gmail.com>
2019-02-20 21:15:58 +0200

    Attempt to add adaptivity to aditii

--- aditii/src/index.js
+++ aditii/src/index.js
@@ -1,3 +1,36 @@
-import Glide from '@glidejs/glide'
+let banner = new Glide('.banner', {
+    type: 'carousel',
+    autoplay: 4000,
+    hoverpause: true,
+}).mount();
+let categories = new Glide('.categories', {
+    type: 'carousel',
+    perView: 3,
+    breakpoints: {
+        1024: {
+            perView: 2
+        },
+        686: {
+            perView: 1
+        }
+    },
+    autoplay: 4000,
+    hoverpause: false
+}).mount();
 
-new Glide('.glide').mount()
+let hamburger = document.querySelector('.hamburger'),
+    nav = document.querySelectorAll('nav a');
+
+hamburger.addEventListener('click', () => {
+    if (hamburger.classList.contains('is-active') == true) {
+        hamburger.classList.remove('is-active');
+        nav.forEach(element => {
+            element.classList.remove('show__nav');
+        });
+    } else {
+        hamburger.classList.add('is-active');
+        nav.forEach(element => {
+            element.classList.add('show__nav');
+        });
+    }
+});
