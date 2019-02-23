e07e87110664bbc973f5f16169ffba22f4af03b4
vladbu <vladbuinytski@gmail.com>
2019-02-23 14:08:20 +0200

    Reworked aditii products to vue.js

--- aditii/src/index.js
+++ aditii/src/index.js
@@ -1,8 +1,10 @@
+// banner slider
 let banner = new Glide('.banner', {
     type: 'carousel',
     autoplay: 4000,
     hoverpause: true,
 }).mount();
+// categories slider
 let categories = new Glide('.categories', {
     type: 'carousel',
     perView: 3,
@@ -18,6 +20,7 @@ let categories = new Glide('.categories', {
     hoverpause: false
 }).mount();
 
+// menu button for mobile
 let hamburger = document.querySelector('.hamburger'),
     nav = document.querySelectorAll('nav a');
 
@@ -33,4 +36,44 @@ hamburger.addEventListener('click', () => {
             element.classList.add('show__nav');
         });
     }
+});
+
+// products
+let products = new Vue({
+    el: '#products',
+    data: {
+        products: [
+            {
+                title: 'branded shoe',
+                price: '$300',
+                image: './img/product1.png'
+            },
+            {
+                title: 'branded tees',
+                price: '$300',
+                image: './img/product2.png'
+            },
+            {
+                title: 'branded tees',
+                price: '$300',
+                image: './img/product3.png'
+            },
+            {
+                title: 'branded wallet',
+                price: '$300',
+                image: './img/product4.png'
+            },
+            {
+                title: 'ems woman bag',
+                price: '$300',
+                image: './img/product5.png'
+            },
+            {
+                title: 'branded cargos',
+                price: '$300',
+                image: './img/product6.png'
+            },
+
+        ],    
+    },
 });
