fcd6e102bd428fea2c0807b9289fca31163e091d
vladbu <vladbuinytski@gmail.com>
2019-03-13 15:13:10 +0200

    added vue carousel

--- aditii-vue/src/components/Categories.vue
+++ aditii-vue/src/components/Categories.vue
@@ -1,37 +1,27 @@
 <template>
-    <section class="categories" data-parallax="scroll">
-        <div class="glide__arrows" data-glide-el="controls">
-            <button class="glide__arrow glide__arrow--left" data-glide-dir="<"></button>
-        </div>
-        <div class="glide__track" data-glide-el="track">
-            <ul class="glide__slides">
-                <li class="glide__slide">
-                    <img src="../assets/img/categories1.png" alt="">
-                    <div>
-                        <h2>branded shoes</h2>
-                        <button>shop</button>
-                    </div>
-                </li>
-                <li class="glide__slide">
-                    <img src="../assets/img/categories2.png" alt="">
-                    <div>
-                        <h2>branded tshirts</h2>
-                        <button>shop</button>
-                    </div>
-                </li>
-                <li class="glide__slide">
-                    <img src="../assets/img/categories3.png" alt="">
-                    <div>
-                        <h2>branded shorts</h2>
-                        <button>shop</button>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="glide__arrows" data-glide-el="controls">
-            <button class="glide__arrow glide__arrow--right" data-glide-dir=">"></button>
-        </div>
-    </section>
+    <carousel class="categories" :perPageCustom="[[100, 1], [700, 2], [1024, 3]]" :autoplay="true" :paginationEnabled="false">
+        <slide class="slide">
+            <img src="../assets/img/categories1.png" alt="">
+            <div>
+                <h2>branded shoes</h2>
+                <button>shop</button>
+            </div>
+        </slide>
+        <slide class="slide">
+            <img src="../assets/img/categories2.png" alt="">
+            <div>
+                <h2>branded tshirts</h2>
+                <button>shop</button>
+            </div>
+        </slide>
+        <slide class="slide">
+            <img src="../assets/img/categories3.png" alt="">
+            <div>
+                <h2>branded shorts</h2>
+                <button>shop</button>
+            </div>
+        </slide>
+    </carousel>
 </template>
 
 <script>
@@ -41,7 +31,7 @@
 </script>
 
 <style scoped>
-    section.categories {
+    .categories {
         padding: 0 1%;
         background: white;
         box-shadow: 0px 2px 8px 0px rgba(0,0,0,.2);
@@ -50,25 +40,26 @@
         align-items: center;
         color: #afafaf;
     }
-    section.categories li.glide__slide {
+    .categories .slide {
         height: 150px;
         padding: 1% 0;
         border-right: 1px solid rgba(175, 175, 175, .3);
         display: flex;
+        justify-content: center;
     }
-    section.categories li.glide__slide img {
+    .categories .slide img {
         height: 100%;
         margin: 0 0 0 10%;
     }
-    section.categories li.glide__slide div {
+    .categories .slide div {
         width: 100px;
         margin-left: 5%;
     }
-    section.categories li.glide__slide h2 {
+    .categories .slide h2 {
         font-family: BebasNeue;
         font-size: 2.5em;
     }
-    section.categories li.glide__slide button {
+    .categories .slide button {
         font-size: 2em;
         text-transform: uppercase;
         color: white;
@@ -78,27 +69,27 @@
         padding: 5% 10% 0% 10%;
         cursor: pointer;
     }
-    section.categories button.glide__arrow {
+    .categories button.glide__arrow {
         height: 25vh;
         width: 50px;
         border: none;
         cursor: pointer;
     }
-    section.categories button.glide__arrow--left {
+    .categories button.glide__arrow--left {
         background: url(../assets/img/arrow_left.png) center no-repeat;
         background-size: contain;
     }
-    section.categories button.glide__arrow--right {
+    .categories button.glide__arrow--right {
         background: url(../assets/img/arrow_right.png) center no-repeat;
         background-size: contain;
     }
     @media only screen and (max-width: 940px) {
-        section.categories button.glide__arrow {
+        .categories button.glide__arrow {
             display: none;
         }
     }
     @media only screen and (max-width: 630px) {
-        section.categories button.glide__arrow {
+        .categories button.glide__arrow {
             display: none;
         }
     }
--- aditii-vue/src/main.js
+++ aditii-vue/src/main.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import Routes from './routes'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Routes from './routes';
+import VueCarousel from 'vue-carousel';
 
-Vue.use(VueRouter)
 
-const router = new VueRouter ({
+Vue.use(VueRouter);
+Vue.use(VueCarousel);
+
+
+const router = new VueRouter({
   routes: Routes,
   mode: 'history'
-})
+});
 
 Vue.config.productionTip = false
 
