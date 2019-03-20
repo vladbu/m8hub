3c2f31766bc31f83c960a7477a04c521d19553e6
Vlad Ivanov <vladbuinytski@gmail.com>
2019-03-20 15:11:11 +0200

    todo app.vue tweaks

--- vuetify-todo/src/App.vue
+++ vuetify-todo/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <v-app>
-    <todo/>  
+    <todo/>
   </v-app>
 </template>
 
 <script>
-import Todo from './components/Todo.vue'
+import Todo from "./components/Todo.vue";
 
 export default {
   components: {
-    Todo,
+    Todo
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
